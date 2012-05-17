bc30de116584a3d6c29e545f6c03e107a09497a4
Li Yao <hnkfliyao@gmail.com>
2012-05-17 20:20:55 +0800

    Seperate lines in APPEND.
    Signed-off-by: Li Yao <hnkfliyao@gmail.com>

--- mdexec.py
+++ mdexec.py
@@ -62,7 +62,7 @@ def append_handle(line):
 	line = line[4:]
 	# print line
 	try:
-	    file_object.write(line)
+	    file_object.write(line + '\n')
 	except IOError:
 	    print "Error happened while trying to append text into file: %s" % (file_name)
     else:
